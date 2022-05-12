a9f50ca00060eb5fe8f09add30594253df3383e9
wayofthepie <wayofthepie@gmail.com>
2022-05-12 16:21:35 +0100

    Read secrets without echo

--- src/handle.rs
+++ src/handle.rs
@@ -1,11 +1,12 @@
 use crate::cli::{Action, Args};
 use crate::gpg::Gpg;
+use anyhow::Context;
 use std::{
     fs::{File, OpenOptions},
-    io::{Read, Write},
+    io::Write,
 };
 
-pub fn handle<R: Read, P: Persister>(
+pub fn handle<R: SecretReader, P: Persister>(
     mut handler: Handler<R, P>,
     args: &Args,
 ) -> anyhow::Result<HandlerResult> {
@@ -24,7 +25,7 @@ pub struct Handler<R, P> {
     reader: R,
 }
 
-impl<R: Read, P: Persister> Handler<R, P> {
+impl<R: SecretReader, P: Persister> Handler<R, P> {
     pub fn new(persister: P, reader: R) -> Self {
         Self {
             gpg: Gpg::default(),
@@ -34,22 +35,34 @@ impl<R: Read, P: Persister> Handler<R, P> {
     }
 
     pub fn insert(&mut self, name: &str, key_id: &str) -> anyhow::Result<HandlerResult> {
-        let buf = &self.read_in_value()?;
+        let buf = &self.read_in_secret_value()?;
         let ciphertext = self.gpg.encrypt(key_id, buf)?;
         let file = self.write_out_value(name, &ciphertext)?;
         Ok(HandlerResult::Insert(file))
     }
 
-    fn read_in_value(&mut self) -> anyhow::Result<Vec<u8>> {
-        let mut buf = Vec::new();
-        self.reader.read_to_end(&mut buf)?;
-        Ok(buf)
+    fn read_in_secret_value(&mut self) -> anyhow::Result<Vec<u8>> {
+        self.reader.read_secret()
     }
 
     fn write_out_value(&self, name: &str, ciphertext: &[u8]) -> anyhow::Result<File> {
         let mut file = self.persister.create(name)?;
         file.write_all(ciphertext)?;
         Ok(file)
+    }
+}
+
+pub trait SecretReader {
+    fn read_secret(&self) -> anyhow::Result<Vec<u8>>;
+}
+
+pub struct StdinSecretReader;
+
+impl SecretReader for StdinSecretReader {
+    fn read_secret(&self) -> anyhow::Result<Vec<u8>> {
+        let secret = rpassword::prompt_password("Enter your secret: ")
+            .with_context(|| "failed to read from input source")?;
+        Ok(secret.into_bytes())
     }
 }
 
@@ -68,9 +81,8 @@ impl OnDiskPersister {
 impl Persister for OnDiskPersister {
     fn create(&self, name: &str) -> anyhow::Result<File> {
         OpenOptions::new()
-            .create_new(true)
+            .create(true)
             .write(true)
-            .append(true)
             .open(name)
             .map_err(|e| e.into())
     }
@@ -78,7 +90,7 @@ impl Persister for OnDiskPersister {
 
 #[cfg(test)]
 mod test {
-    use super::Persister;
+    use super::{Persister, SecretReader};
     use crate::{
         cli::Action,
         gpg::{
@@ -90,11 +102,22 @@ mod test {
     };
     use memfile::CreateOptions;
     use std::{
+        cell::RefCell,
         fs::File,
         io::{Read, Seek, SeekFrom},
         path::Path,
     };
     use tempfile::tempdir;
+
+    struct FakeSecretReader<'a> {
+        secret: RefCell<&'a [u8]>,
+    }
+
+    impl<'a> SecretReader for FakeSecretReader<'a> {
+        fn read_secret(&self) -> anyhow::Result<Vec<u8>> {
+            Ok(rpassword::read_password_from_bufread(&mut self.secret.take())?.into_bytes())
+        }
+    }
 
     struct InMemoryPersister;
 
@@ -126,12 +149,15 @@ mod test {
             },
         };
         let mut buf = Vec::new();
-        let input = "value";
-        let handler = Handler::new(InMemoryPersister, input.as_bytes());
+        let input = "password\n";
+        let secret_reader = FakeSecretReader {
+            secret: RefCell::new(input.as_bytes()),
+        };
+        let handler = Handler::new(InMemoryPersister, secret_reader);
         let HandlerResult::Insert(mut file) = handle(handler, &args).expect("expected a result");
         file.seek(SeekFrom::Start(0)).unwrap();
         file.read_to_end(&mut buf).unwrap();
         let plaintext = gpg.decrypt(&buf).unwrap();
-        assert_eq!(plaintext, input);
+        assert_eq!(plaintext, input.trim());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,12 @@ mod handle;
 
 use clap::Parser;
 use cli::Args;
-use handle::handle;
-use handle::OnDiskPersister;
-use handle::{Handler, HandlerResult};
+use handle::{handle, Handler, HandlerResult, OnDiskPersister, StdinSecretReader};
 
 fn main() -> anyhow::Result<()> {
-    let handler = Handler::new(OnDiskPersister::new(), std::io::stdin());
+    let handler = Handler::new(OnDiskPersister::new(), StdinSecretReader);
     match handle(handler, &Args::parse())? {
-        HandlerResult::Insert(_) => println!("Success"),
+        HandlerResult::Insert(_) => println!("Secret saved."),
     }
     Ok(())
 }
