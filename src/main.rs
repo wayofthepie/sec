d9d8b427965701bb63deaba4b45dadfbcaf5c12c
wayofthepie <wayofthepie@gmail.com>
2022-05-15 11:03:22 +0100

    Refactor to use store

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,15 @@ pub mod store;
 
 use clap::Parser;
 use cli::Args;
-use input::{handle, Handler, OnDiskPersister};
+use input::{handle, Handler};
 use output::{write_result, TerminalOutput};
 use secrets::StdinSecretReader;
 use std::io;
+use store::OnDiskStore;
 
 fn main() -> anyhow::Result<()> {
     let output = TerminalOutput::new(io::stdout());
-    let mut handler = Handler::new(OnDiskPersister::new(), StdinSecretReader);
+    let mut handler = Handler::new(OnDiskStore::new("~/.sec_store"), StdinSecretReader);
     let result = handle(&mut handler, &Args::parse())?;
     write_result(result, output)
 }
